459c4fb2d68c42d6504cd535b46a9a97c30f03a1
williamtng <williamtong1207@gmail.com>
2025-01-08 20:40:57 +0100

    08/01 W2 malloc on envp and unset working

--- src/utils/init.c
+++ src/utils/init.c
@@ -6,7 +6,7 @@
 /*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 14:34:29 by arotondo          #+#    #+#             */
-/*   Updated: 2025/01/07 14:59:31 by witong           ###   ########.fr       */
+/*   Updated: 2025/01/08 20:21:01 by witong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ void	init_shell(t_shell *shell, int argc, char **argv, char **envp)
 	shell->argc = argc;
 	shell->exit_status = 0;
 	shell->argv = argv;
-	if (envp)
-		shell->envp = envp;
-	// else
-	// 	shell->envp = ;
+	shell->envp = arraydup(envp);
 	shell->token = NULL;
 	shell->cmd = NULL;
 	shell->clean = NULL;
